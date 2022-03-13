fb2c88e27a2be4d9d05ac82ae1ad94f88a200424
DELL <DELL@Chinnu>
2022-03-13 22:55:47 +0530

    First hibernate

---
+++ HibernateFirst/src/main/java/com/tectoro/HibernateFirst/App.java
@@ -0,0 +1,31 @@
+package com.tectoro.HibernateFirst;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        System.out.println(factory);
+        System.out.println("completed");
+        
+        Student st = new Student(2, "Chinnu", "CMR");
+        
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(st);
+        transaction.commit();
+        
+    }
+}
---
+++ JavaPrograms/src/JavaPrograms/StringReplace.java
@@ -0,0 +1,29 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class StringReplace {
+	public static void main(String[] args) {
+		/*
+		 * StringBuilder st = new StringBuilder("Prashanth"); Scanner s = new
+		 * Scanner(System.in); //st = s.next(); //StringBuilder st = new
+		 * StringBuilder(s.next()); System.out.println("hi"); StringBuilder sb = new
+		 * StringBuilder(s.next()); //sb=s.next(); System.out.println(sb); sb=new
+		 * StringBuilder(s.next()); System.out.println("After changing");
+		 * System.out.println(sb);
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		System.out.println(s);
+		
+		System.out.println("If you want to change please enter");
+		s=sc.next();
+		System.out.println(s);
+		
+		
+		
+		
+		
+	}
+}
